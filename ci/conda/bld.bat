7c3e48728c6742dedc591dbfb21157ca9e99c0e0
Thomas Paviot <tpaviot@gmail.com>
2019-11-07 09:25:35 +0100

    force bld.bat to use msvc

--- ci/conda/bld.bat
+++ ci/conda/bld.bat
@@ -5,7 +5,7 @@ REM Remove dot from PY_VER for use in library name
 set MY_PY_VER=%PY_VER:.=%
 
 REM Configure step
-cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64 cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
  -DPYTHONOCC_BUILD_TYPE=RelWithDebInfo ^
  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
  -DCMAKE_SYSTEM_PREFIX_PATH="%LIBRARY_PREFIX%" ^
